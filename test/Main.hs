edefd2e2874ebcc4a3492ede28ad3cce03befb99
Ozgun Ataman <ozataman@gmail.com>
2013-03-15 17:07:33 -0400

    Update test script

--- test/Main.hs
+++ test/Main.hs
@@ -12,7 +12,7 @@ simpleTest = do
   f <- B.lines `fmap` B.readFile "data/city_test.txt"
   let ips = map (head . B.split '#') f
 
-  db <- openGeoDB memory_cache "data/GeoLiteCity.dat"
-  let rs = map (geoLocateByIPAddress db) ips
+  db <- openGeoDB memory_cache "/usr/local/share/GeoIP/GeoIPCity.dat"
+  rs <- mapM (geoLocateByIPAddress db) ips
   putStrLn . show $ rs
   
